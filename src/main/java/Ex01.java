f43f86fb6df23b28357106852be73cf6996150c5
Leonardo Barreto <lapb@icomp.ufam.edu.br>
2020-09-17 20:31:27 -0400

    sol: exerc01
    Signed-off-by: Leonardo Barreto <lapb@icomp.ufam.edu.br>

---
+++ src/main/java/Ex01.java
@@ -0,0 +1,9 @@
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Na tabela Projeto falta a coluna de status descrita na contextualização.\nEssa coluna deve ser do tipo INT para assumir os 3 valores possíveis:\n0 - aguardando recurso; 1 - executando; 2 - finalizado.");
+	}
+
+}
